ce652ec6af7ddf200d5d87061e11b525dcade3e7
Liubou Kavaleuskaya <LKavaleuskaya@gomel.iba.by>
2021-11-20 22:58:32 +0300

    init: create app * Delete uncorrect created app * Create app correctly

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/vpupkin-ang2'),
+      dir: require('path').join(__dirname, './coverage/AngularJS'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'vpupkin-ang2'`, () => {
+  it(`should have as title 'AngularJS'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('vpupkin-ang2');
+    expect(app.title).toEqual('AngularJS');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('vpupkin-ang2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularJS app is running!');
   });
 });
